b39634309d00ef95ae00814bdca428be15eb8e23
Mohamed Ashif K M <mohamedashif1911@gmail.com>
2024-09-02 20:32:53 +0530

    yes

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,8 +1,9 @@
 import Countdown from "./components/Countdown"
+import bg from '../public/bg1.jpg' 
 const App = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-cover bg-bottom"
-    style={{ backgroundImage: `url('../../public/bg1.jpg')` }}
+    style={{ backgroundImage: `url(${bg})` }}
     >
        <h1 className="text-5xl mt-5 text-white">Hackathon Name</h1>
        <Countdown />
